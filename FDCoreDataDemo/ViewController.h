49572de70b90c9107dad86086f929351a51caeaa
kenanwangyi <674904111@qq.com>
2017-04-17 17:34:59 +0800

    Initial Commit

---
+++ FDCoreDataDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FDCoreDataDemo
+//
+//  Created by 伏董 on 2017/4/17.
+//  Copyright © 2017年 伏董. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FDCoreDataDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FDCoreDataDemo
+//
+//  Created by 伏董 on 2017/4/17.
+//  Copyright © 2017年 伏董. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
